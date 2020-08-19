ef20a0e45a67faa144697f5c7a0cd54743b28a34
Vincent Laferriere <github@monordibogue.com>
2020-08-19 19:06:59 -0400

    added htmldecoder for chucknorris

--- src/commands/chucknorris/index.ts
+++ src/commands/chucknorris/index.ts
@@ -1,10 +1,13 @@
 import DiscordJS from "discord.js";
 import fetch from "node-fetch";
 
+import { htmldecode } from "../../util";
+
 const icndb = (nb: number) =>
     fetch(`https://api.icndb.com/jokes/${nb === -1 ? "random" : nb}`)
         .then((r) => r.json())
-        .then((json) => json.value.joke);
+        .then((json) => json?.value?.joke)
+        .then((joke) => (joke ? htmldecode(joke) : null));
 
 export default async (message: DiscordJS.Message, ...args: string[]) => {
     return message.reply(
--- src/util.ts
+++ src/util.ts
@@ -7,3 +7,28 @@ export const waitFor = (
     stream: { once: (event: string, ...args: any[]) => any },
     event: string
 ) => new Promise((resolve) => stream.once(event, resolve));
+
+const htmlentities: any = {
+    nbsp: " ",
+    cent: "¢",
+    pound: "£",
+    yen: "¥",
+    euro: "€",
+    copy: "©",
+    reg: "®",
+    lt: "<",
+    gt: ">",
+    quot: '"',
+    amp: "&",
+    apos: "'",
+};
+
+export const htmldecode = (str: string) =>
+    str.replace(/&(.+?);/g, (substr: string, ...args: string[]) => {
+        if (htmlentities[args[0]]) return htmlentities[args[0]];
+        else if (args[0].indexOf("#x") === 0)
+            return String.fromCharCode(parseInt(args[0].substr(2), 16) || 63);
+        else if (args[0].indexOf("#") === 0)
+            return String.fromCharCode(parseInt(args[0].substr(1)) || 63);
+        else return substr;
+    });
