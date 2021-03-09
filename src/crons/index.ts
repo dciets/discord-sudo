9f1b520adb5c12f852844c0f7de6ba59ef818a37
Vincent Laferriere <github@monordibogue.com>
2021-03-09 10:03:38 -0500

    removed pogchamp

--- src/crons/index.ts
+++ src/crons/index.ts
@@ -1,14 +1,3 @@
 import { Client } from "discord.js";
-import cron from "cron";
 
-import pogchamp from "./pogchamp";
-
-export function startCronJobs(client: Client) {
-    new cron.CronJob(
-        "0 0 * * * *",
-        pogchamp(client),
-        null,
-        true,
-        "America/Montreal"
-    );
-}
+export function startCronJobs(client: Client) {}
