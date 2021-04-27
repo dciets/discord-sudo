4553340c1e8b83d66b2dbf61e67394284cb9cfb4
Emile Filteau-Tessier <emile.filteau@gmail.com>
2021-04-26 21:53:08 -0400

    Add BlazeIt Voice message once again

---
+++ src/crons/blazeit.ts
@@ -0,0 +1,21 @@
+import { Client, VoiceChannel } from "discord.js";
+import { waitFor } from "../util";
+
+export default function (client: Client) {
+    return () => {
+        client.guilds?.cache.each(async ({ channels }) => {
+            const generalVoice = channels.cache.find(
+                (c) => c.type === "voice" && c.name === "Général"
+            ) as VoiceChannel;
+            if (!generalVoice) return;
+
+            const connection = await generalVoice.join();
+            const dispatcher = connection.play(
+                "./assets/audio/smokeweed-everyday.mp3"
+            );
+            await waitFor(dispatcher, "finish");
+            dispatcher.end();
+            generalVoice.leave();
+        });
+    };
+}
